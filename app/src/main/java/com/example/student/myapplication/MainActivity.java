4e9a5ebf23616f3ff16dda2fd5d185271ef251cd
acei <acei1212@gmail.com>
2017-10-05 10:34:42 +0800

    新增 Dialog 5s

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.student.myapplication;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb,pb2;
+    ProgressBar pb, pb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
         pb2 = (ProgressBar) findViewById(R.id.progressBar2);
-
     }
 
     public void click1(View v) {
@@ -47,13 +47,39 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
             }.start();
-        }}
-
-    public void click4(View v){
-        pb2.setProgress(pb2.getProgress()+10);
-    }
-    public void click5(View v){
-        pb2.setProgress(pb2.getProgress()-10);
+        }
     }
 
+    public void click4(View v) {
+        pb2.setProgress(pb2.getProgress() + 10);
+    }
+
+    public void click5(View v) {
+        pb2.setProgress(pb2.getProgress() - 10);
+    }
+
+    public void click6(View v) {
+        final ProgressDialog pd = new ProgressDialog(MainActivity.this);
+        pd.setMessage("Please Wait");
+        pd.setCancelable(false);
+        pd.show();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        pd.dismiss();
+                    }
+                });
+            }
+        }.start();
+    }
 }
+
